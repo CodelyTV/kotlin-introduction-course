e13bcfe6dfad7db70ad8a095ca2aa256f6aa51a4
Isabel Garrido <isabel.garrido@code.seat>
2021-08-02 22:23:11 +0200

    Add example for null handling

---
+++ 02-02-null-handling/src/main/kotlin/com/codely/demo/App.kt
@@ -0,0 +1,18 @@
+package com.codely.demo
+
+import java.time.LocalDate
+
+class App
+
+fun main() {
+    println("Please enter a date with the format <yyyy-MM-dd>")
+    supportNullableString(readLine())?.let {
+        val date = LocalDate.parse(it)
+        println("You wrote the date $date")
+    }
+    println("Bye!")
+}
+
+private fun supportNullableString(line: String?) = line
+private fun forceNotNullByThrowingNullPointerException(line: String?) = line!!
+private fun forceNotNullByThrowingSpecifiedException(line: String?) = line ?: throw RuntimeException("The date cannot be null")
