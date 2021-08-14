0fb4c95ef76d0a3ebec722a98cfaf79a5a04e635
Isabel Garrido <isabel.garrido@code.seat>
2021-08-14 18:26:03 +0200

    Add example for if vs when block

---
+++ 03-02-if-vs-when/src/main/kotlin/com/codely/demo/App.kt
@@ -0,0 +1,39 @@
+package com.codely.demo
+
+import java.time.LocalDate
+import java.time.Period
+import kotlin.system.exitProcess
+
+class App
+
+fun main() {
+    println("Please enter a date with the format <yyyy-MM-dd>")
+    supportNullableString(readLine())
+    .takeUnless {
+        it.isNullOrEmpty()
+    }?.let {
+        LocalDate.parse(it)
+    }.apply {
+        if (this == null) {
+            println("The introduced date <$this> is not valid")
+            exitProcess(1)
+        }
+    }.also {
+        println("You wrote the date $it")
+    }.takeIf {
+            it != null
+    }?.run {
+        val currentDate = LocalDate.now()
+        kotlin.with(Period.between(this, currentDate)) {
+            when {
+                this.years > 0 -> println("The difference between the date you wrote an today is ${this.years} years")
+                this.months > 0 -> println("The difference between the date you wrote an today is ${this.months} months")
+                this.days > 0 -> println("The difference between the date you wrote an today is ${this.days} days")
+            }
+        }
+    }
+
+    println("Bye!")
+}
+
+private fun supportNullableString(line: String?) = line
