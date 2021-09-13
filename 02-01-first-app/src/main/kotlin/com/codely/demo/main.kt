47a7f0addc543ed1c764fcdb254389ec768aa0ad
Isabel Garrido <isabel.garrido@code.seat>
2021-09-13 15:56:18 +0200

    Some improvements to make the examples more clear

--- 02-01-first-app/src/main/kotlin/com/codely/demo/main.kt
+++ 02-01-first-app/src/main/kotlin/com/codely/demo/main.kt
@@ -2,8 +2,6 @@ package com.codely.demo
 
 import java.time.LocalDate
 
-class App
-
 fun main() {
     println("Please enter a date with the format <yyyy-MM-dd>")
     val date = LocalDate.parse(readLine())
--- 05-01-data-classes/src/main/kotlin/com/codely/demo/main.kt
+++ 05-01-data-classes/src/main/kotlin/com/codely/demo/main.kt
@@ -1,9 +1,10 @@
 package com.codely.demo
 
 import com.codely.demo.app.App
+import com.codely.demo.app.Clock
 import com.codely.demo.shared.Reader
 import com.codely.demo.shared.Writer
 
 fun main() {
-    App(Reader(), Writer()).execute()
+    App(Reader(), Writer(), Clock()).execute()
 }
